fe769f47cac0064a1f967a79d72a092620dd6904
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2022-08-08 00:51:20 +0800

    Feat: Add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app  = express();
 const cors = require('cors');
+
+require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
 /* Middlewares */
