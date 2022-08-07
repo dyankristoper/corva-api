9688621d257192caa8eef75893722a6865f59b06
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2022-08-07 14:52:25 +0800

    Feat: Add coral router

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app  = express();
 const PORT = process.env.PORT || 8000;
 
+/* Routers */
+const shallowCoralRouter = require('./routes/corals');
+
 app.get('/', (request,response)=>{ response.send(`API running!`); })
 
+// Routes
+app.use('/api/v1/shallow-coral', shallowCoralRouter );
+ 
 app.listen( PORT , () => { console.log(`App is running on port ${PORT}`); })
