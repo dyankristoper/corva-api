6560b8b6fa3b5135716e5e60e248deee6d9f174e
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2022-08-07 16:37:44 +0800

    Feat: Add CORS

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app  = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 8000;
+
+/* Middlewares */
+app.use(cors());
 
 /* Routers */
 const shallowCoralRouter = require('./routes/corals');
