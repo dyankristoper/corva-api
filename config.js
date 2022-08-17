2458b40fd6f74b5952e70fa43016536645cda51c
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2022-08-17 15:18:02 +0800

    Feat: Testing SQL Connection

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host:     'localhost',
+    host:     process.env.ENV == 'DEVELOPMENT' ? `${process.env.DB_HOST}:3306` : 'localhost',
     user:     process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
-connection.connect( () => { console.log(`DB Connection has been established!`); })
 
 module.exports = connection;
--- routes/corals.js
+++ routes/corals.js
@@ -6,20 +6,32 @@ const DB_NAME = 'coral_shallow';
 
 router.get('/:id', ( request, response ) => {
     /* Date time logs of requests */
-    console.log(`GET -- /${request.params.id} | ${ Date.now().toLocaleString() }`);
+    console.log(`GET -- /${request.params.id} | ${ new Date() }`);
 
-    connection.query(`SELECT * FROM ${DB_NAME} WHERE loc_id = ${request.params.id}` , ( err, data ) => {
-        const coralData = [
-           { 'Type': 'Hard Coral', 'Count': data.HC_cover },
-           { 'Type': 'Algal Assemblage', 'Count': data.AA_cover },
-           { 'Type': 'Abiotic',          'Count': data.AB_cover },
-           { 'Type': 'Macroalgae',       'Count': data.MA_cover },
-           { 'Type': 'Halimeda',    'Count': data.HA_cover },
-           { 'Type': 'Other Biota', 'Count': data.OB_cover },
-        ];
+    // connection.query(`SELECT * FROM ${DB_NAME} WHERE loc_id = ${request.params.id}` , ( err, data ) => {
+    //     const coralData = [
+    //        { 'Type': 'Hard Coral', 'Count': data.HC_cover },
+    //        { 'Type': 'Algal Assemblage', 'Count': data.AA_cover },
+    //        { 'Type': 'Abiotic',          'Count': data.AB_cover },
+    //        { 'Type': 'Macroalgae',       'Count': data.MA_cover },
+    //        { 'Type': 'Halimeda',    'Count': data.HA_cover },
+    //        { 'Type': 'Other Biota', 'Count': data.OB_cover },
+    //     ];
 
-        response.send( coralData );
+    //     response.send( coralData );
+    // });
+
+    
+    connection.connect( (err) => {
+        if(err){ console.log(err); }
     });
+
+    const query = `SELECT 1 + 1 AS Result`;
+    connection.query( query, ( err, result ) => {
+        console.log( result );
+    });
+
+    connection.end();
 });
 
 module.exports = router;
