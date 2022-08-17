f5bf33e0cbc0ebb3055efa0c1b46cbfeb8bf0f1a
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2022-08-17 15:37:55 +0800

    Fix: Updated query on per loc per year

--- routes/corals.js
+++ routes/corals.js
@@ -33,17 +33,19 @@ router.get('/:id/:year', ( request, response ) => {
     });
     
     const query = `SELECT * FROM ${ CORAL_DB_NAME } WHERE loc_id = ${ request.params.id } AND monitor_year = ${ request.params.year }`;
-    connection.query( query , ( err, data ) => {
-        const coralData = [
-           { 'Type': 'Hard Coral', 'Count': data.HC_cover },
-           { 'Type': 'Algal Assemblage', 'Count': data.AA_cover },
-           { 'Type': 'Abiotic',          'Count': data.AB_cover },
-           { 'Type': 'Macroalgae',       'Count': data.MA_cover },
-           { 'Type': 'Halimeda',    'Count': data.HA_cover },
-           { 'Type': 'Other Biota', 'Count': data.OB_cover },
-        ];
+    connection.query( query , ( err, results ) => {
+        console.log( results );
 
-        response.send( coralData );
+        // const coralData = [
+        //    { 'Type': 'Hard Coral', 'Count': data.HC_cover },
+        //    { 'Type': 'Algal Assemblage', 'Count': data.AA_cover },
+        //    { 'Type': 'Abiotic',          'Count': data.AB_cover },
+        //    { 'Type': 'Macroalgae',       'Count': data.MA_cover },
+        //    { 'Type': 'Halimeda',    'Count': data.HA_cover },
+        //    { 'Type': 'Other Biota', 'Count': data.OB_cover },
+        // ];
+
+        // response.send( coralData );
     });
 
     connection.end();
